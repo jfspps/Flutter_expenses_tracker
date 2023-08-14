17a944b91a636e514705b509a277ee9cc357b2e4
James Apps <james.apps@energise.energy>
2023-08-14 14:52:44 +0100

    added TextInputControllers to monitor data entry

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
@@ -5,23 +6,62 @@ class NewExpense extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-    return _NewExpense();
+    return _NewExpenseState();
   }
 }
 
-class _NewExpense extends State<NewExpense> {
+class _NewExpenseState extends State<NewExpense> {
+  // takes care of text input when the modal screen is required
+  final _textInputController = TextEditingController();
+  final _numericalInputController = TextEditingController();
+
+  // remove the widget when done (not in view) from the modal page
+  @override
+  void dispose() {
+    _numericalInputController.dispose();
+    _textInputController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Column(
-        children: const [
+        children: [
           TextField(
+            controller: _textInputController,
             maxLength: 50,
-            decoration: InputDecoration(
-              label: Text('Enter text here'),
+            decoration: const InputDecoration(
+              label: Text('Enter expense title'),
             ),
           ),
+          TextField(
+            controller: _numericalInputController,
+            maxLength: 10,
+            keyboardType: const TextInputType.numberWithOptions(decimal: true),
+            decoration: const InputDecoration(
+              prefixText: '£ ',
+              label: Text('Enter amount'),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  if (kDebugMode) {
+                    print('Expense title: ${_textInputController.text}');
+                    print('Amount: ${_numericalInputController.text}');
+                  }
+                },
+                child: const Text('Save'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
+              ),
+            ],
+          )
         ],
       ),
     );
