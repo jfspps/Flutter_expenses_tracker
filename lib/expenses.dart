7e2adf53e3fb62e998acee97ef5fa0db09575f0d
James Apps <james.apps@energise.energy>
2023-08-15 12:37:44 +0100

    added UNDO operation (undo swipe removal) with a snackbar widget

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -17,45 +17,86 @@ class _Expenses extends State<Expenses> {
   final List<Expense> expensesList = [];
 
   void _openAddExpenseOverlay() {
-      // a modalBottomSheet receives temporary focus at the bottom of the screen,
-      // greying out other widgets; to revert focus, the user clicks the greyed-out area;
-      // the context stores metadata that relates this widget to all others in a
-      // tree-like data structure; "context" below is global and is distinct from "ctx"
-      // which is specific to the ModalBottomSheet widget
-      showModalBottomSheet(
-        context: context,
-        // forces modal page to use entire page and can be scrolled close
-        isScrollControlled: true,
-        builder: (ctx) => NewExpense(_addExpense),
-      );
+    // a modalBottomSheet receives temporary focus at the bottom of the screen,
+    // greying out other widgets; to revert focus, the user clicks the greyed-out area;
+    // the context stores metadata that relates this widget to all others in a
+    // tree-like data structure; "context" below is global and is distinct from "ctx"
+    // which is specific to the ModalBottomSheet widget
+    showModalBottomSheet(
+      context: context,
+      // forces modal page to use entire page and can be scrolled close
+      isScrollControlled: true,
+      builder: (ctx) => NewExpense(_addExpense),
+    );
   }
 
-  void _addExpense(Expense expense){
+  void _addExpense(Expense expense) {
     setState(() {
       expensesList.add(expense);
       if (kDebugMode) {
-        print("Added expense: ${expense.title} dated ${expense.formattedDate}");
+        print(
+          "Added expense: ${expense.title} dated ${expense.formattedDate}",
+        );
       }
     });
   }
 
-  void _removeExpense(Expense expense){
+  void _removeExpense(Expense expense) {
     setState(() {
       expensesList.remove(expense);
       if (kDebugMode) {
-        print("Removed expense: ${expense.title} dated ${expense.formattedDate}");
+        print(
+          "Removed expense: ${expense.title} dated ${expense.formattedDate}",
+        );
       }
+
+      // make sure all related snack bars are removed from the UI (users can swipe
+      // multiple times)
+      ScaffoldMessenger.of(context).removeCurrentSnackBar();
+
+      // now inform the user and allow them to reverse the changes;
+      // note that UNDO will change the order of the elements in the list
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          duration: const Duration(
+            seconds: 2,
+          ),
+          content: const Text(
+            'Expense item removed',
+          ),
+          action: SnackBarAction(label: 'UNDO', onPressed: () {
+            setState(() {
+              _addExpense(expense);
+            });
+          }),
+        ),
+      );
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget expensesListWidget = const Center(
+      child: Text(
+        'No expenses saved',
+      ),
+    );
+
+    if (expensesList.isNotEmpty) {
+      expensesListWidget = ExpensesList(
+        expenses: expensesList,
+        removeExpense: _removeExpense,
+      );
+    }
+
     // Scaffold has prebuilt widgets to manage the title bar and other commonly
     // used widgets
     return Scaffold(
       // AppBar takes care of space above the AppBar
       appBar: AppBar(
-        title: const Text('Expenses tracking app'),
+        title: const Text(
+          'Expenses tracking app',
+        ),
         actions: [
           IconButton(
             onPressed: _openAddExpenseOverlay,
@@ -71,10 +112,7 @@ class _Expenses extends State<Expenses> {
             "Expenses so far...",
           ),
           Expanded(
-            child: ExpensesList(
-              expenses: expensesList,
-              removeExpense: _removeExpense,
-            ),
+            child: expensesListWidget,
           ),
         ],
       ),
